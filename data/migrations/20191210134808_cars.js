f5f64721b748dbb16478c32e335e4f3f1315907a
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2019-12-10 14:19:00 -0600

    seeds file added, migration table populated

--- data/migrations/20191210134808_cars.js
+++ data/migrations/20191210134808_cars.js
@@ -2,12 +2,12 @@
 exports.up = function(knex) {
     return knex.schema.createTable('cars', table => {
         table.increments();
-        table.integer('VIN').notNullable().unique().index()
+        table.string('VIN').notNullable().unique().index()
         table.string('Make', 500).notNullable().index()
         table.string('Model', 500).notNullable().index()
         table.integer('Mileage').notNullable().unique().index()
-        table.string('Transmission Type', 500).index()
-        table.string('Title Status', 500).index()
+        table.string('Transmission_Type', 500).index()
+        table.string('Title_Status', 500).index()
     })
 };
 
